7b3e7b1c058445d11b14c2824c9ca5a81d87bb93
march <106459595+marchc1@users.noreply.github.com>
2026-07-04 23:59:39 -0700

    imgui

--- example_modules/example5_network_property_modding/module.cpp
+++ example_modules/example5_network_property_modding/module.cpp
@@ -14,7 +14,7 @@ static const char* kTargetProp = "m_iHealth";
 static RecvVarProxyFn g_originalHealthProxy = nullptr;
 
 static void HealthRecvProxy(const CRecvProxyData* pData, void* pStruct, void* pOut) {
-	g_api->Log("example", "%s.%s <- %d (object %d)\n", kTargetClass, kTargetProp, pData->m_Value.m_Int, pData->m_ObjectID);
+	g_api->Log("example5", "%s.%s <- %d (object %d)\n", kTargetClass, kTargetProp, pData->m_Value.m_Int, pData->m_ObjectID);
 	if (g_originalHealthProxy)
 		g_originalHealthProxy(pData, pStruct, pOut);
 }
@@ -22,24 +22,24 @@ static void HealthRecvProxy(const CRecvProxyData* pData, void* pStruct, void* pO
 static void RewriteClientClasses(ClientClassesEditor& editor) {
 	ClientClassEditor cls = editor.GetClass(kTargetClass);
 	if (!cls.Valid()) {
-		g_api->Log("example", "could not find client class %s -- available:\n", kTargetClass);
+		g_api->Log("example5", "could not find client class %s -- available:\n", kTargetClass);
 		editor.ForEachClass([](const char* name) { g_api->Log("example", "  %s\n", name); });
 		return;
 	}
 
-	g_api->Log("example", "props on %s:\n", kTargetClass);
+	g_api->Log("example5", "props on %s:\n", kTargetClass);
 	cls.ForEachProp([](const char* name, int type, int offset, int flags) {
-		g_api->Log("example", "  %s : %s (offset %d, flags 0x%X)\n", name, NetPropTypeName(type), offset, flags);
+		g_api->Log("example5", "  %s : %s (offset %d, flags 0x%X)\n", name, NetPropTypeName(type), offset, flags);
 		});
 
 	RecvPropEditor health = cls.GetProp(kTargetProp);
 	if (health.Valid()) {
 		g_originalHealthProxy = health.OriginalProxy();
 		health.RerouteProxy(&HealthRecvProxy);
-		g_api->Log("example", "rerouted %s.%s recv proxy\n", kTargetClass, kTargetProp);
+		g_api->Log("example5", "rerouted %s.%s recv proxy\n", kTargetClass, kTargetProp);
 	}
 	else {
-		g_api->Log("example", "could not find %s.%s\n", kTargetClass, kTargetProp);
+		g_api->Log("exampl5e", "could not find %s.%s\n", kTargetClass, kTargetProp);
 	}
 }
 
@@ -52,12 +52,12 @@ static void RewriteServerClasses(ServerClassesEditor& editor) {
 	if (origin.Valid()) {
 		origin.SetFlags(0);
 		origin.AddFlags(SPROP_NOSCALE);
-		g_api->Log("example", "set CBaseEntity.m_vecOrigin to noscale (flags now 0x%X)\n", origin.Flags());
+		g_api->Log("example5", "set CBaseEntity.m_vecOrigin to noscale (flags now 0x%X)\n", origin.Flags());
 	}
 }
 
 MODULE_MAIN() {
-	g_api->Log("example", "Loaded Example Module #5: Modifying RecvProp/SendProps (abi %u)\n", api->abiVersion);
+	g_api->Log("example5", "Loaded Example Module #5: Modifying RecvProp/SendProps (abi %u)\n", api->abiVersion);
 	api->AddClientClassRewriter(&RewriteClientClasses);
 	api->AddServerClassRewriter(&RewriteServerClasses);
 } MODULE_END()
